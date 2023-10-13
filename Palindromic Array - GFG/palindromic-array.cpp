e1cc55fd72acc700e392cdf8ff49c4e1c02a493f
Pooja Kumari <120014006+KPoojapatel@users.noreply.github.com>
2023-10-13 18:34:11 +0530

    Added solution - LeetHub

---
+++ Palindromic Array - GFG/palindromic-array.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*Complete the function below*/
+
+class Solution {
+public:
+    int PalinArray(int a[], int n)
+   {
+        for (int i = 0; i < n; i++) {
+            int num = a[i];
+            int original = num;
+            int reversed = 0;
+            while (num > 0) {
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num /= 10;
+            }
+            if (original != reversed) {
+                return 0; // If any element is not a palindrome, return 0.
+            }
+        }
+        return 1; // All elements are palindromes, so return 1.
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i = 0; i < n; i++)
+			cin>>a[i];
+		Solution obj;
+		cout<<obj.PalinArray(a,n)<<endl;
+	}
+}
+// } Driver Code Ends
