b23c8d8130fd6d7ec3ca1d11034bfffd8c8e14f1
Pooja Kumari <120014006+KPoojapatel@users.noreply.github.com>
2023-10-16 18:09:10 +0530

    Added solution - LeetHub

---
+++ The dice problem - GFG/the-dice-problem.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int oppositeFaceOfDice(int N)
+    {  
+        for(int i=1;i<=6;i++){
+            if(N+i==7)
+                return i;
+            
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        int ans  = ob.oppositeFaceOfDice(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
