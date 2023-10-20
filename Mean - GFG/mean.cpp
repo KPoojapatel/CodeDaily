d0515fbfd27d77211d1dfae55761903e18aac082
Pooja Kumari <120014006+KPoojapatel@users.noreply.github.com>
2023-10-20 18:37:10 +0530

    Added solution - LeetHub

---
+++ Mean - GFG/mean.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int mean(int N , int A[]) {
+        int count=0;
+        int sum=0;
+        for(int i=0;i<N;i++){
+            sum=sum+A[i];
+            count++;
+        }
+        int mean= sum/count;
+        return mean;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.mean(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
